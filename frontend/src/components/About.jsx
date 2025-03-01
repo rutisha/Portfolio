c35fed0b0f75f2410aaa775c2c23274f35945d4e
Rutisha Patel <patelrutisha@gmail.com>
2025-03-01 18:26:28 -0500

    new updates

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -7,18 +7,26 @@ const About = () => {
             <h2>About</h2>
             <span></span>
             <p>
-                Hey there, I am Rutisha Patel, a passionate web developer who is interested in learning things each and every day.
-                I have completed my bachelor's of technology in the field of computer engineering. 
-                So I have a basic understanding of website development, programming, digital marketing, and more. 
-                My coding toolbox includes HTML, CSS, and JavaScript for the front end, and I'm still in the learning phase for back-end development. 
+               Hey there, I am Rutisha Patel, a passionate web developer who is interested in learning things each and every day.
+                I have completed my bachelor's of technology in the field of computer engineering and Ontario Grudaute Certificate in Web Development. 
+                So I have a great understanding of website development, programming, digital marketing, and more. 
+                My coding toolbox includes HTML, CSS, and JavaScript for the front end, and back-end development such as Node.js, MySQl, Mongodb. 
                 Moreover, I have some experience and mastery of Shopify frameworks. 
                 Join me as I explore the fascinating world of web development!
             </p>
             <div className="about-section">
                 <div className="experience">
-                    <h3>Experience <i class="fa fa-history" aria-hidden="true"></i></h3>
+                    <h3>Experience </h3>
                     <span></span>
                     <ul>
+                    <li>
+                            <h4>Web Developer Intern</h4>
+                            <p><i class="fa fa-map-marker" aria-hidden="true"></i><strong> Brand Glow Up</strong> - Toronto, Ontario </p>
+                            <p><i class="fa fa-calendar" aria-hidden="true"></i> Sept 2024 - Dec 2024</p>
+                            <p>- Developed and maintained responsive websites, ensuring optimal user experience across various devices and browsers.</p>
+                           <p> - Implemented SEO best practices, resulting in a great improvement in search engine rankings for client websites. </p>
+                           <p> - Created and maintain various blogs on WordPress site</p>
+                        </li>
                         <li>
                             <h4>Web Developer </h4>
                             <p><i class="fa fa-map-marker" aria-hidden="true"></i><strong> WebPixel Technologies</strong> - Vastrapur,Gujarat, India </p>
@@ -34,13 +42,19 @@ const About = () => {
                 </div>
                 
                 <div className="education">
-                    <h3>Education <i class="fa fa-book" aria-hidden="true"></i></h3>
+                    <h3>Education </h3>
                     <span></span>
                     <ul>
+                    <li>
+                            <h4>Master's of Computer Science</h4>
+                            <p><i class="fa fa-map-marker" aria-hidden="true"></i><strong> Algoma University</strong> - Toronto, Ontario, Canada</p>
+                            <p>Present</p>
+                            <p>Studying a program which focused on AI and Machine Learning products.With other software engineering subjects.</p>
+                        </li>
                         <li>
                             <h4>Post-Graduate Diploma Certificate in Web Development</h4>
                             <p><i class="fa fa-map-marker" aria-hidden="true"></i><strong> Humber College </strong> - Toronto, Ontario, Canada</p>
-                            <p>Currently Studying</p>
+                            <p>Passed in 2024 with 90 CGPA</p>
                             <p>Studying an intensive program focused on full-stack development using modern technologies.</p>
                         </li>
                         <li>
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../index.css';
-import Myimage from '../assets/IMG-20200719-WA0322.jpg';
+import Myimage from '../assets/profile-photo.jpeg';
 import About from './About'; 
 import Skills from './Skills'; 
 const Home = () => {
@@ -22,9 +22,9 @@ const Home = () => {
             <span></span>
             <p>
                 Hey there, I am Rutisha Patel, a passionate web developer who is interested in learning things each and every day.
-                I have completed my bachelor's of technology in the field of computer engineering. 
-                So I have a basic understanding of website development, programming, digital marketing, and more. 
-                My coding toolbox includes HTML, CSS, and JavaScript for the front end, and I'm still in the learning phase for back-end development. 
+                I have completed my bachelor's of technology in the field of computer engineering and Ontario Grudaute Certificate in Web Development. 
+                So I have a great understanding of website development, programming, digital marketing, and more. 
+                My coding toolbox includes HTML, CSS, and JavaScript for the front end, and back-end development such as Node.js, MySQl, Mongodb. 
                 Moreover, I have some experience and mastery of Shopify frameworks. 
                 Join me as I explore the fascinating world of web development!
             </p>
